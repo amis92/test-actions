c0a853d721e0134420d5f2e20b1e5866ff217722
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-08-20 19:41:13 +0200

    Update action.ps1

--- .github/actions/composite/action.ps1
+++ .github/actions/composite/action.ps1
@@ -1,11 +1,11 @@
 
 Write-Host "Running composite action"
-$stagingPath = (Get-ChildItem "Env:INPUT_STAGING-PATH").Value
-Write-Host "Staging path input: $stagingPath"
+# $stagingPath = (Get-ChildItem "Env:INPUT_STAGING-PATH").Value
+# Write-Host "Staging path input: $stagingPath"
 Write-Host "Env variables:"
 
 foreach ($item in (Get-ChildItem Env:))
 {
     Write-Host "$($item.Name)=$($item.Value)"
 }
-Write-Host "::set-output name=staging-path::$stagingPath"
+# Write-Host "::set-output name=staging-path::$stagingPath"
