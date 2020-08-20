809b53f14798b3509fda7fc5a558859c656d07ad
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-08-20 19:40:40 +0200

    Update action.ps1

--- .github/actions/composite/action.ps1
+++ .github/actions/composite/action.ps1
@@ -2,4 +2,10 @@
 Write-Host "Running composite action"
 $stagingPath = (Get-ChildItem "Env:INPUT_STAGING-PATH").Value
 Write-Host "Staging path input: $stagingPath"
+Write-Host "Env variables:"
+
+foreach ($item in (Get-ChildItem Env:))
+{
+    Write-Host "$($item.Name)=$($item.Value)"
+}
 Write-Host "::set-output name=staging-path::$stagingPath"
